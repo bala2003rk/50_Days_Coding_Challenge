326c7726b7ee1a7d0c378da36319056369e0a71f
bala2003rk <muruganrajendran23@gmail.com>
2025-08-11 22:05:29 +0530

    Added Day 15 code

---
+++ Day-15/Longest_Palindromic_Substring.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static boolean isPalindrome(String str){
+        int i=0;
+        int j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public String getLongestPal(String s) {
+        int n=s.length();
+        int i=0,j=0,max=1;
+        StringBuilder sb=new StringBuilder(s.substring(0,1));
+        for(i=0;i<=n-max;i++){
+            for(j=i+max;j<n;j++){
+                if(isPalindrome(s.substring(i,j+1))){
+                    sb.setLength(0);
+                    sb.append(s.substring(i,j+1));
+                    max=sb.length();
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
+
