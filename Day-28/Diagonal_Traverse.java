ec6baefb1ee2c0466c4f76a5cf9fe551417d3a73
bala2003rk <muruganrajendran23@gmail.com>
2025-08-27 20:56:26 +0530

    Added Day 28 code

---
+++ Day-28/Diagonal_Traverse.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] result = new int[rows * cols];
+        int r = 0, c = 0;
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = matrix[r][c];
+
+            if ((r + c) % 2 == 0) {
+                if (c == cols - 1) {
+                    r++;
+                } else if (r == 0) {
+                    c++;
+                } else {
+                    r--;
+                    c++;
+                }
+            }
+            else {
+                if (r == rows - 1) {
+                    c++;
+                } else if (c == 0) {
+                    r++;
+                } else {
+                    r++;
+                    c--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
